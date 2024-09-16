05a0a19590b7134fa8b7c87b815917b5191db643
Devdynamow <amanrathore9630@gmail.com>
2024-09-16 13:16:39 +0530

    react boiler plate

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <>
+    <h1>{import.meta.env.VITE_API_KEY}</h1>
+
+    
+      <h1>hello from pata nahi react se</h1>
+    </>
+
+  );
+}
+
+
+
+// Export both components
+export { App };
